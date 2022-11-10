f176be1a33af5ddb22d9233518ff165f49e9ac66
GitHub Action <action@github.com>
2022-11-10 15:17:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/patrickchense/yamlvalidator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/patrickchense/yamlvalidator/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.patrickchense.yamlvalidator.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.patrickchense.yamlvalidator.MyBundle
 
 class MyProjectService(project: Project) {
 
